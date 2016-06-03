d9073f75bddaafe2eccba0689f692033a1994e47
Jessica <hanlind@andrew.cmu.edu>
2016-06-03 16:50:24 -0400

    06/03 account folder added

---
+++ account/account.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MATERIAL_DIRECTIVES} from 'ng2-material';
+import { MdCard } from '@angular2-material/card';
+import { MdCardHeader } from '@angular2-material/card';
+
+s
+@Component({
+  selector: 'account',
+  templateUrl: '/client/imports/account/account.html',
+  directives: [ MATERIAL_DIRECTIVES]
+})
+
+export class Account { }
